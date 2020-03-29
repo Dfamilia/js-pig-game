403e1d863c373ba520d2a2560b77c752179c6725
dfamilia <dfamilia@intellisys.com.do>
2020-03-29 00:38:50 -0400

    DOM manipulation examples

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var scores, roundScores, activePlayer, dice;
+
+scores = [0, 0];
+roundScores = 0;
+activePlayer = 0;
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(`#current-${activePlayer}`).textContent = dice;
+
+var x = document.querySelector("#score-0").textContent;
+
+// you can use querySelecto to select styles also
+document.querySelector(".dice").style.display = "none";
