037b67e974ccc940791cc2aaeeaa6e9c52fc467f
dfamilia <dfamilia@intellisys.com.do>
2020-03-29 02:01:48 -0400

    updating scores and changing active player

--- app.js
+++ app.js
@@ -8,16 +8,47 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScores, activePlayer, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0, 0];
 roundScores = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector(`#current-${activePlayer}`).textContent = dice;
-
-var x = document.querySelector("#score-0").textContent;
 
 // you can use querySelecto to select styles also
 document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+document.querySelector(".btn-roll").addEventListener("click", () => {
+  // 0. Local variables declarations
+  var dice, currentPlayerDOM, diceDOM;
+  diceDOM = document.querySelector(".dice");
+  currentPlayerDOM = document.getElementById(`current-${activePlayer}`);
+
+  // 1. Variables Assigments
+  dice = Math.floor(Math.random() * 6) + 1;
+
+  // 2. Display the results
+  diceDOM.style.display = "block";
+  diceDOM.src = `dice-${dice}.png`;
+
+  // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    // Add score
+    roundScores += dice;
+    currentPlayerDOM.textContent = roundScores;
+  } else {
+    // Reset roundScore, reset currentPlayer roundScore, Next player
+    roundScores = 0;
+    currentPlayerDOM.textContent = roundScores;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    document.querySelector(`.player-0-panel`).classList.toggle("active");
+    document.querySelector(`.player-1-panel`).classList.toggle("active");
+
+    diceDOM = document.querySelector(".dice").style.display = "none";
+  }
+});
